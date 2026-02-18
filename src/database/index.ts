7212012bb72811c979219a393502a3ca1429986a
MohanK-pixel <mohnk007@gmail.com>
2026-02-18 17:23:19 +0530

    Codebase migration from javascript to typescript

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 import { db } from "../config.js"
 
-const dbURI = `mongodb://${db.user}:${db.password}@${db.host}:${db.port}/${db.name}`
+const dbURI = `mongodb://${db.host}:${db.port}/${db.name}`
 
 mongoose
   .connect(dbURI)
